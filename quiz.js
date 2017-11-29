0e57605bd2e6c0fb85b60db8a562cbc4272f6834
ASMO360 <uafredrickson@gmail.com>
2017-11-29 11:48:02 -0800

    single page quiz functionality works

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ var dayNum;
 var mainEl = document.getElementById('main');
 var done = false;
 var selanswr;
-
+var questionCounter = 1;
 (function() {
   user = JSON.parse(localStorage.getItem('current'));
   dayNum = parseInt(user.day.slice(-1));
@@ -39,51 +39,38 @@ function showQuestion(day, qNum){
   var temp = document.getElementById(tempStr);
   temp.style.display = 'block';
   temp.addEventListener('submit', recordAnswer);
-  wait();
+  //wait();
 }
 
-function wait() {
-  if(done === false) {
-    window.setTimeout(wait, 100);
-  }
-}
+// function wait() {
+//   if(done === false) {
+//     window.setTimeout(wait, 100);
+//   }
+// }
 
 function recordAnswer(event) {
+  event.preventDefault();
+
   done = true;
-  if(event.target.a1.value === 'on') {
+  if(event.target.answer.value === 'a1') {
     selanswr = 1;
-  } else if (event.target.a2.value === 'on') {
+  } else if (event.target.answer.value === 'a2') {
     selanswr = 2;
-  } else if(event.target.a3.value === 'on') {
+  } else if(event.target.answer.value === 'a3') {
     selanswr = 3;
-  } else if(event.target.a4.value === 'on') {
+  } else if(event.target.answer.value === 'a4') {
     selanswr = 4;
+  }
+  event.target.style.display = 'none';
+  questionCounter++;
+  if(questionCounter <= 10) {
+    showQuestion(dayNum, questionCounter);
   }
 }
 
 function single() {
-  //debugger;
-  if(dayNum === 1){
-    for(var i = 1; i <= 10; i++){
-      showQuestion(1, i);
-    }
-  } else if(dayNum === 2){
-    for(var i = 1; i <= 10; i++){
-      showQuestion(2, i);
-    }
-  } else if(dayNum === 3){
-    for(var i = 1; i <= 10; i++){
-      showQuestion(3, i);
-    }
-  } else if(dayNum === 4){
-    for(var i = 1; i <= 10; i++){
-      showQuestion(4, i);
-    }
-  } else if(dayNum === 5){
-    for(var i = 1; i <= 10; i++){
-      showQuestion(5, i);
-    }
-  }
+  questionCounter = 1;
+  showQuestion(dayNum, 1);
 }
 
 var formEl = document.getElementById('form');
